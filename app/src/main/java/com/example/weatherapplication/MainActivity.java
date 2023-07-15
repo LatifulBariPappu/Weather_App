1fcc44535ff42f0e63a80dd8fbfe1bc96b626bc0
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-07-15 20:41:33 +0600

    fontend

--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.weatherapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText cityName,cityCode;
+    Button getButton;
+    TextView resultTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cityName=findViewById(R.id.search_city_edit_text);
+        cityCode=findViewById(R.id.search_citycode_edit_text);
+        getButton=findViewById(R.id.getBtn);
+        resultTv=findViewById(R.id.resultTv);
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+            }
+        });
     }
 }
