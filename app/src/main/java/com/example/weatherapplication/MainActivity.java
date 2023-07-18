97e1e6452b008bb3a140d1c13dc8c61f36c39d83
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-07-18 23:38:17 +0600

    day4

--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.weatherapplication;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +16,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 
@@ -56,7 +62,30 @@ public class MainActivity extends AppCompatActivity {
             StringRequest stringRequest=new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    Log.d("response",response);
+                    try {
+                        JSONObject jsonResponse=new JSONObject(response);
+                        JSONArray jsonArray=jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather=jsonArray.getJSONObject(0);
+                        String description=jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain=jsonResponse.getJSONObject("main");
+                        double temp=jsonObjectMain.getDouble("temp")-273.15;
+                        double feelslike=jsonObjectMain.getDouble("feels_like")-273.15;
+                        float pressure=jsonObjectMain.getInt("pressure");
+                        int humidity=jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind=jsonResponse.getJSONObject("wind");
+                        String wind=jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectClouds=jsonResponse.getJSONObject("clouds");
+                        String clouds=jsonObjectClouds.getString("all");
+                        JSONObject jsonObjectSys=jsonResponse.getJSONObject("sys");
+                        String countryName=jsonObjectSys.getString("country");
+                        String cityName=jsonResponse.getString("name");
+                        resultTv.setTextColor(Color.rgb(68,134,199));
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
                 }
             }, new Response.ErrorListener() {
                 @Override
